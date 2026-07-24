dd3dda044ed565674590a1f8d3e86af112ff00d4
Manoj-1945 <manojkumargk289@gmail.com>
2026-07-24 14:29:52 +0000

    token-tokens

---
+++ frontend/src/theme/tokens.ts
@@ -0,0 +1,22 @@
+export const colors = {
+  void: '#0B0E14',
+  circuit: '#141924',
+  circuitLight: '#1C2333',
+  copper: '#FF9F45',
+  current: '#2DD4BF',
+  signal: '#FF5470',
+  success: '#3DDC84',
+  mist: '#7A88A6',
+  mistDim: '#4A5470',
+  white: '#F5F7FA',
+};
+
+export const fonts = {
+  display: 'ChakraPetch_600SemiBold',
+  displayBold: 'ChakraPetch_700Bold',
+  body: 'ChakraPetch_400Regular',
+  mono: 'SpaceMono_400Regular',
+};
+
+export const radii = { sm: 8, md: 14, lg: 20, full: 999 };
+export const spacing = { xs: 4, sm: 8, md: 16, lg: 24, xl: 32 };
